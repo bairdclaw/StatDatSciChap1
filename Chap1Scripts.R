8fe893bb99edcfb8d104add8383f13cfc8051286
Loren Baird <lornie@gmail.com>
2018-08-13 12:11:31 -0500

    First Script

---
+++ Chap1Scripts.R
@@ -0,0 +1,4 @@
+state<-read.csv(file="C:/Dev/GitWorkspace/StatDatSciChap1/StatDatSciChap1/data/state.csv")
+mean(state[["Population"]])
+mean(state[["Population"]], trim=0.1)
+median(state[["Population"]])
