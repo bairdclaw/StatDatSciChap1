945ae612ecb85e6e1f883c5af97b1c5e8498bdb4
Loren Baird <lornie@gmail.com>
2018-08-13 12:18:45 -0500

    Additional Chap 1.  WeightedMean and importing a library

--- Chap1Scripts.R
+++ Chap1Scripts.R
@@ -2,3 +2,7 @@ state<-read.csv(file="C:/Dev/GitWorkspace/StatDatSciChap1/StatDatSciChap1/data/s
 mean(state[["Population"]])
 mean(state[["Population"]], trim=0.1)
 median(state[["Population"]])
+
+weighted.mean(state[["Murder.Rate"]], w=state[["Population"]])
+library(matrixStats)
+weightedMedian(state[["Murder.Rate"]], w=state[["Population"]])
